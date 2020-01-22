ba44fe9ef8f27db4ae466844fa64ccbc67c31226
Andriy Veremyeyev and neha doodi <andriy@>
2020-01-22 15:07:34 -0800

    add if condition o find out if a particular yr is a leap yr

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function() {
+    $("form#leap-year").submit(function(event) 
+    {
+        event.preventDefault();
+        var year = parseInt($("input#year").val());
+        
+        // function leapYear(year) {
+        //     if (year % 4 === 0) {
+        //         return true;
+        //       } else {
+        //         return false;
+        //     }
+        // }
+        
+        var leapYear = function(year) {
+            if ((year % 4 === 0) && ((year % 100 !== 0) || (year % 400 === 0))) {
+              return true;
+            } else {
+              return false;
+            }
+          };
+
+        var result = leapYear(year);
+        $("#result").text(result);
+    });
+});
